7ff09d37637ea685aae6cf6d8990798843bda001
danyangelova <daniela.ant.ang@gmail.com>
2025-12-31 14:55:16 +0200

    Wrap app with ThemeProvider

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./style/style.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ThemeProvider } from "./context/ThemeContext.jsx";
 
 const queryClient = new QueryClient();
 
@@ -11,7 +12,9 @@ createRoot(document.getElementById("root")).render(
    <StrictMode>
       <QueryClientProvider client={queryClient}>
          <BrowserRouter>
-            <App />
+            <ThemeProvider>
+               <App />
+            </ThemeProvider>
          </BrowserRouter>
       </QueryClientProvider>
    </StrictMode>
